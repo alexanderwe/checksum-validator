5759e0d8d69d26c592a222498d67b6816d72cf8c
Alexander <weiss_alex@gmx.net>
2017-11-05 23:51:14 +0100

    Modiy webpack config to accept environment variables Add first .travis.yml file, but not tested Add travis-build.sh to make spectron work on travis ci Add spectron base tests

---
+++ test/test.ts
@@ -0,0 +1,35 @@
+
+import * as chai from 'chai';
+import { assert, expect } from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import * as electron from 'electron';
+import * as mocha from 'mocha';
+import { Application } from 'spectron';
+
+chai.use(chaiAsPromised);
+chai.should();
+
+beforeEach(() => {
+    this.app = new Application({
+        args: ['app/main/build/main.js'],
+        path: 'node_modules/.bin/electron',
+    });
+
+    return this.app.start().then((app) => {
+        return app;
+    });
+});
+
+afterEach(() => {
+    if (this.app && this.app.isRunning()) {
+        return this.app.stop();
+    }
+});
+
+describe('Base tests', () => {
+    it('shows an initial window', () => {
+        return this.app.client.getWindowCount().then((count) => {
+            assert.equal(count, 1);
+        });
+    });
+});
