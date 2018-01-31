c3bfd2f168bbda549dc068001f2bfd9064dfaaa5
Alexander Weiß <weiss_alex@gmx.net>
2018-01-31 22:58:00 +0100

    Add antd and make first steps

--- app/renderer/src/components/Checksum.component.tsx
+++ app/renderer/src/components/Checksum.component.tsx
@@ -2,17 +2,24 @@ import { ipcRenderer } from 'electron';
 import * as React from 'react'; // ES6
 import Transition from 'react-transition-group/Transition';
 
-import Button from './bulma/element/Button.component';
-import Icon from './bulma/element/Icon.component';
-
+import FadeAndSlideDownTransition from './transition/FadeAndSlideDownTransition.component';
 import Notification from './bulma/element/Notification.component';
+import BulmaIcon from './bulma/element/Icon.component';
+
+/*
+import BulmaButton from './bulma/element/Button.component';
+
+
 import Form from './bulma/form/Form.component';
 import Container from './bulma/layout/Container.component';
 import Hero from './bulma/layout/hero/Hero.component';
 import Section from './bulma/layout/Section.component';
-import FadeAndSlideDownTransition from './transition/FadeAndSlideDownTransition.component';
 
 import Tag from './bulma/element/Tag.component';
+import Button from 'antd/lib/button';
+*/
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
 
 import I18n from '../../../lib/i18n/I18n';
 
@@ -56,39 +63,43 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
       notificationOpen: false,
       saveChecksum: false,
       type: 'SHA256',
-      updateMsg: null,
+      updateMsg: null
     };
   }
 
   public componentDidMount() {
-    ipcRenderer.on('checksum-result', (event: any, data: any) => {
-      this.setState(
-        {
-          checksumResult: data.checksumResult,
-          error: data.error,
-          loading: false,
-          match: data.match,
-        },
-        this.openNotification(),
-      );
-    }).on('check', (event: any, data: any) => {
-      this.check();
-    }).on('update', (event: any, data: any) => {
-      this.setState({
-        checkingForUpdate: false,
-        updateMsg: data,
+    ipcRenderer
+      .on('checksum-result', (event: any, data: any) => {
+        this.setState(
+          {
+            checksumResult: data.checksumResult,
+            error: data.error,
+            loading: false,
+            match: data.match
+          },
+          this.openNotification()
+        );
+      })
+      .on('check', (event: any, data: any) => {
+        this.check();
+      })
+      .on('update', (event: any, data: any) => {
+        this.setState({
+          checkingForUpdate: false,
+          updateMsg: data
+        });
+      })
+      .on('checkForUpdate', (event: any, data: any) => {
+        this.setState({
+          checkingForUpdate: true
+        });
       });
-    }).on('checkForUpdate', (event: any, data: any) => {
-      this.setState({
-        checkingForUpdate: true,
-      });
-    });
 
     document.body.ondrop = (event: any) => {
       this.setState({
         fileHover: false,
         fileName: event.dataTransfer.files[0].name,
-        filePath: event.dataTransfer.files[0].path,
+        filePath: event.dataTransfer.files[0].path
       });
       event.preventDefault();
     };
@@ -96,13 +107,13 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
     document.ondragover = document.ondrop = (event: any) => {
       event.preventDefault();
       this.setState({
-        fileHover: true,
+        fileHover: true
       });
     };
     document.ondragend = document.ondrop = (event: any) => {
       event.preventDefault();
       this.setState({
-        fileHover: false,
+        fileHover: false
       });
     };
 
@@ -110,7 +121,7 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
       event.preventDefault();
       if (event.target.className === 'hero-body') {
         this.setState({
-          fileHover: false,
+          fileHover: false
         });
       }
     };
@@ -118,7 +129,7 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
 
   public handleSelectChange = (event: any) => {
     this.setState({ type: event.target.value });
-  }
+  };
 
   public handleInputChange = (event: any) => {
     const target = event.target;
@@ -126,127 +137,98 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
     const name = target.name;
 
     this.setState({
-      [name]: value,
+      [name]: value
     });
-  }
+  };
 
   public handleFileChange = (event: any) => {
-    this.setState(
-      {
-        fileName: event.target.files[0].name,
-        filePath: event.target.files[0].path,
-      },
-    );
-  }
+    this.setState({
+      fileName: event.target.files[0].name,
+      filePath: event.target.files[0].path
+    });
+  };
 
   public handleCheckboxChange = (event: any) => {
     this.setState({
-      saveChecksum: event.target.checked,
+      saveChecksum: event.target.checked
     });
-  }
+  };
 
   public closeNotification = (): any => {
     this.setState({
-      notificationOpen: false,
+      notificationOpen: false
     });
-  }
+  };
 
   public openNotification = (): any => {
     this.setState({
-      notificationOpen: true,
+      notificationOpen: true
     });
-  }
+  };
 
   public updateApp = (): any => {
     ipcRenderer.send('update-app', {});
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
   }
 
   public check = () => {
-
     if (this.state.filePath !== '' && this.state.checksum !== '') {
       this.closeNotification();
       this.setState({
-        loading: true,
+        loading: true
       });
       setTimeout(() => {
         ipcRenderer.send('checksum', {
           checksum: this.state.checksum,
           filepath: this.state.filePath,
           saveChecksum: this.state.saveChecksum,
-          type: this.state.type,
+          type: this.state.type
         });
       }, 1000);
     }
-  }
+  };
 
   public render() {
     const button = <div>Button</div>;
-
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div className='checksum-validator'>
-        <FadeAndSlideDownTransition in={!!this.state.notificationOpen} duration={500}>
-          <Notification isSuccess={this.state.match} isDanger={!this.state.match || this.state.error} style={{ width: '100%', position: 'absolute', zIndex: '10' }} onCloseClick={() => this.closeNotification()}>
-            {!this.state.checksumResult ? <div>
-              <Icon name={'nc-alert-exc'} style={{ marginRight: '10px' }} /> {i18n.translate('checksum fail')}
-            </div> : this.state.match ? (
-              <div>
-                <Icon name={'nc-check-2'} style={{ marginRight: '10px' }} /> {i18n.translate('checksum match')}
-              </div>
-            ) : (
-                  <div>
-                    <Icon name={'nc-alert-exc'} style={{ marginRight: '10px' }} /> {i18n.translate('checksum mismatch')}
-                  </div>
-                )}
-          </Notification>
-        </FadeAndSlideDownTransition>
-        <div className='dimmer' style={this.state.fileHover ? { display: 'block' } : { display: 'none' }}>
-          <Hero isMedium isFullHeight>
-            <Hero.Body>
-              <Container>
-                <h1 className='title' style={{ color: '#fff' }}>
-                  {i18n.translate('drop file')}
-                </h1>
-                <Icon name={'nc-file-download-89'} isLarge />
-              </Container>
-            </Hero.Body>
-          </Hero>
-        </div>
-        <Section>
-          <Form.Field>
-            <Form.File isBoxed isPrimary isFullWidth label={i18n.translate('choose file')} onChange={this.handleFileChange} fileName={this.state.fileName} />
-          </Form.Field>
-          <Form.Field hasIconsLeft>
-            <input className='input' type='text' name='checksum' placeholder='Checksum' value={this.state.checksum} onChange={this.handleInputChange} />
-            <Icon name={'nc-code'} isSmall isLeft />
-          </Form.Field>
-          <Form.Field hasIconsLeft>
-            <span className='select'>
-              <select value={this.state.type} onChange={this.handleSelectChange}>
-                <option value='SHA256'>SHA256</option>
-                <option value='SHA512'>SHA512</option>
-                <option value='SHA1'>SHA1</option>
-                <option value='MD5'>MD5</option>
-              </select>
-            </span>
-            <Icon name={'nc-security'} isSmall isLeft />
-          </Form.Field>
-          <Button isOutLined isPrimary onClick={() => this.check()} icon={this.state.loading ? <Icon name={'nc-dots'} isSmall spin style={{ marginRight: '10px' }} /> : null}>
-            {i18n.translate('check')}
-          </Button>
-        </Section>
-        {this.state.updateMsg ?
-          <Tag
-            isPrimary={this.state.updateMsg.updateAvailable}
-            isDanger={this.state.updateMsg.error}
-            style={{ cursor: 'pointer' }}
-            onClick={this.state.updateMsg.updateAvailable ? () => this.updateApp() : null}>
-            {this.state.checkingForUpdate ? <Icon name={'nc-circle'} spin></Icon> : null}  {this.state.updateMsg.msg}
-          </Tag>
-          : null}
-
-      </div >
+      <div className="checksum-validator">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <FormItem>
+            {getFieldDecorator('userName', {
+              rules: [{ required: true, message: 'Please input your username!' }]
+            })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }]
+            })(<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />)}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true
+            })(<Checkbox>Remember me</Checkbox>)}
+            <a className="login-form-forgot" href="">
+              Forgot password
+            </a>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </FormItem>
+        </Form>
+      </div>
     );
   }
 }
 
-export default ChecksumValidator;
+export default Form.create()(ChecksumValidator);
--- app/renderer/src/entry.tsx
+++ app/renderer/src/entry.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './components/App';
 
 import './styles/app.scss';
+import 'antd/dist/antd.css';
 
 window.onload = () => {
   ReactDOM.render(<App />, document.getElementById('app'));
