a60b0321603efcab09017d4ed4e7f3f3fad5e88e
Alexander Weiß <weiss_alex@gmx.net>
2018-02-02 12:10:47 +0100

    Add sidebar

--- app/renderer/src/components/App.tsx
+++ app/renderer/src/components/App.tsx
@@ -1,15 +1,71 @@
 import * as React from 'react';
 import Container from './bulma/layout/Container.component';
 import Hero from './bulma/layout/hero/Hero.component';
-import ChecksumValidator from './Checksum.component';
+
+import Layout from 'antd/lib/layout';
+import Menu from 'antd/lib/menu';
+import Icon from 'antd/lib/icon';
+
+import ChecksumComponent from './Checksum.component';
+const { Header, Content, Footer, Sider } = Layout;
 
 function app() {
+  const collapsed = true;
+
   return (
-    <Hero isFullHeight>
-      <Container isFluid hasTextCentered isMarginless>
-        <ChecksumValidator />
-      </Container>
-    </Hero>
+    <Layout>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          maxWidth: '80px !important',
+        }}
+        collapsed={true}
+      >
+        <div className="logo" />
+        <Menu mode="inline" theme="dark">
+          <Menu.Item key="1">
+            <Icon type="user" />
+            <span className="nav-text">nav 1</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="video-camera" />
+            <span className="nav-text">nav 2</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="upload" />
+            <span className="nav-text">§6</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Icon type="bar-chart" />
+            <span className="nav-text">nav 4</span>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <Icon type="cloud-o" />
+            <span className="nav-text">nav 5</span>
+          </Menu.Item>
+          <Menu.Item key="6">
+            <Icon type="appstore-o" />
+            <span className="nav-text">nav 6</span>
+          </Menu.Item>
+          <Menu.Item key="7">
+            <Icon type="team" />
+            <span className="nav-text">nav 7</span>
+          </Menu.Item>
+          <Menu.Item key="8">
+            <Icon type="shop" />
+            <span className="nav-text">nav 8</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout style={{ marginLeft: '80px', height: '100vh' }}>
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <ChecksumComponent />
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
--- app/renderer/src/components/Checksum.component.tsx
+++ app/renderer/src/components/Checksum.component.tsx
@@ -18,8 +18,18 @@ import Section from './bulma/layout/Section.component';
 import Tag from './bulma/element/Tag.component';
 import Button from 'antd/lib/button';
 */
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+
+import Form from 'antd/lib/form';
+import Icon from 'antd/lib/icon';
+import Input from 'antd/lib/input';
+import Button from 'antd/lib/button';
+import Checkbox from 'antd/lib/checkbox';
+import Uploader from 'antd/lib/upload';
+import Select from 'antd/lib/select';
+
+const Dragger = Uploader.Dragger;
 const FormItem = Form.Item;
+const Option = Select.Option;
 
 import I18n from '../../../lib/i18n/I18n';
 
@@ -63,7 +73,7 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
       notificationOpen: false,
       saveChecksum: false,
       type: 'SHA256',
-      updateMsg: null
+      updateMsg: null,
     };
   }
 
@@ -75,9 +85,9 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
             checksumResult: data.checksumResult,
             error: data.error,
             loading: false,
-            match: data.match
+            match: data.match,
           },
-          this.openNotification()
+          this.openNotification(),
         );
       })
       .on('check', (event: any, data: any) => {
@@ -86,12 +96,12 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
       .on('update', (event: any, data: any) => {
         this.setState({
           checkingForUpdate: false,
-          updateMsg: data
+          updateMsg: data,
         });
       })
       .on('checkForUpdate', (event: any, data: any) => {
         this.setState({
-          checkingForUpdate: true
+          checkingForUpdate: true,
         });
       });
 
@@ -99,7 +109,7 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
       this.setState({
         fileHover: false,
         fileName: event.dataTransfer.files[0].name,
-        filePath: event.dataTransfer.files[0].path
+        filePath: event.dataTransfer.files[0].path,
       });
       event.preventDefault();
     };
@@ -107,13 +117,13 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
     document.ondragover = document.ondrop = (event: any) => {
       event.preventDefault();
       this.setState({
-        fileHover: true
+        fileHover: true,
       });
     };
     document.ondragend = document.ondrop = (event: any) => {
       event.preventDefault();
       this.setState({
-        fileHover: false
+        fileHover: false,
       });
     };
 
@@ -121,14 +131,14 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
       event.preventDefault();
       if (event.target.className === 'hero-body') {
         this.setState({
-          fileHover: false
+          fileHover: false,
         });
       }
     };
   }
 
-  public handleSelectChange = (event: any) => {
-    this.setState({ type: event.target.value });
+  public handleSelectChange = (value: any) => {
+    this.setState({ type: value });
   };
 
   public handleInputChange = (event: any) => {
@@ -137,32 +147,32 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
     const name = target.name;
 
     this.setState({
-      [name]: value
+      [name]: value,
     });
   };
 
   public handleFileChange = (event: any) => {
     this.setState({
       fileName: event.target.files[0].name,
-      filePath: event.target.files[0].path
+      filePath: event.target.files[0].path,
     });
   };
 
   public handleCheckboxChange = (event: any) => {
     this.setState({
-      saveChecksum: event.target.checked
+      saveChecksum: event.target.checked,
     });
   };
 
   public closeNotification = (): any => {
     this.setState({
-      notificationOpen: false
+      notificationOpen: false,
     });
   };
 
   public openNotification = (): any => {
     this.setState({
-      notificationOpen: true
+      notificationOpen: true,
     });
   };
 
@@ -170,60 +180,108 @@ class ChecksumValidator extends React.Component<any, IChecksumValidatorState> {
     ipcRenderer.send('update-app', {});
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log('Received values of form: ', values);
       }
     });
-  }
+  };
 
   public check = () => {
     if (this.state.filePath !== '' && this.state.checksum !== '') {
       this.closeNotification();
       this.setState({
-        loading: true
+        loading: true,
       });
       setTimeout(() => {
         ipcRenderer.send('checksum', {
           checksum: this.state.checksum,
           filepath: this.state.filePath,
           saveChecksum: this.state.saveChecksum,
-          type: this.state.type
+          type: this.state.type,
         });
       }, 1000);
     }
   };
 
   public render() {
-    const button = <div>Button</div>;
+    const draggerProps = {
+      name: 'file',
+      multiple: false,
+      onChange(info) {
+        const status = info.file.status;
+        if (status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+          console.log(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+          console.log(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
+
     const { getFieldDecorator } = this.props.form;
     return (
       <div className="checksum-validator">
         <Form onSubmit={this.handleSubmit} className="login-form">
+          <Dragger {...draggerProps}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="inbox" />
+            </p>
+            <p className="ant-upload-text">Click or drag to insert a file</p>
+            <p className="ant-upload-hint">
+              Please insert only files and not directories
+            </p>
+          </Dragger>
           <FormItem>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }]
-            })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
+            {getFieldDecorator('checksum', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please provide a checksum to check against',
+                },
+              ],
+            })(
+              <Input
+                style={{ marginTop: '24px' }}
+                prefix={
+                  <BulmaIcon
+                    name={'nc-code'}
+                    isSmall
+                    isLeft
+                    style={{ color: 'rgba(0,0,0,.25)' }}
+                  />
+                }
+                placeholder="Checksum"
+              />,
+            )}
           </FormItem>
           <FormItem>
             {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }]
-            })(<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />)}
+              initialValue: 'SHA256',
+              rules: [
+                { required: true, message: 'Please input your Password!' },
+              ],
+            })(
+              <Select onChange={this.handleSelectChange}>
+                <Option value="SHA256">SHA256</Option>
+                <Option value="SHA512">SHA512</Option>
+                <Option value="SHA1">SHA1</Option>
+                <Option value="MD5">MD5</Option>
+              </Select>,
+            )}
           </FormItem>
           <FormItem>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Check
             </Button>
-            Or <a href="">register now!</a>
           </FormItem>
         </Form>
       </div>
